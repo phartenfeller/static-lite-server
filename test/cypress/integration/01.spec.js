7372ce5696dbe8308d1feb7ebb86d38d26210274
phartenfeller <pharti@hotmail.de>
2022-01-28 16:06:40 +0100

    test 404 page

--- test/cypress/integration/01.spec.js
+++ test/cypress/integration/01.spec.js
@@ -1,14 +1,18 @@
 const URL = "http://localhost:3002";
+const NF_URL = URL + "/does-not-exists";
 
 describe("My First Test", () => {
-  it("Visit index page", () => {
+  it("Index shows correct content", () => {
     cy.visit(URL);
 
     cy.get("body").then(($body) => {
       const $header = $body.find("#header");
       expect($header).to.contain("Hello World");
     });
+  });
 
+  it("Check index status and header", () => {
+    cy.visit(URL);
     cy.request("/").then((response) => {
       expect(response.status).to.equal(200);
       expect(response).to.have.property("headers");
@@ -17,6 +21,15 @@ describe("My First Test", () => {
       // headers are lowercase
       expect(response.headers).to.have.property("server");
       expect(response.headers["server"]).to.contain("static-lite-server:");
+    });
+  });
+
+  it("Check default 404 page", () => {
+    cy.visit(URL);
+    cy.request({ url: NF_URL, failOnStatusCode: false }).then((response) => {
+      expect(response.status).to.equal(404);
+
+      expect(response.body).to.contain("Not Found (404)");
     });
   });
 });
