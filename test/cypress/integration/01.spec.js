e5fe9ab5068af7b1e175095234abe30200d156e9
phartenfeller <pharti@hotmail.de>
2022-01-28 16:09:51 +0100

    setup eslint and prettier

--- test/cypress/integration/01.spec.js
+++ test/cypress/integration/01.spec.js
@@ -1,35 +1,35 @@
-const URL = "http://localhost:3002";
-const NF_URL = URL + "/does-not-exists";
+const URL = 'http://localhost:3002';
+const NF_URL = URL + '/does-not-exists';
 
-describe("My First Test", () => {
-  it("Index shows correct content", () => {
+describe('My First Test', () => {
+  it('Index shows correct content', () => {
     cy.visit(URL);
 
-    cy.get("body").then(($body) => {
-      const $header = $body.find("#header");
-      expect($header).to.contain("Hello World");
+    cy.get('body').then(($body) => {
+      const $header = $body.find('#header');
+      expect($header).to.contain('Hello World');
     });
   });
 
-  it("Check index status and header", () => {
+  it('Check index status and header', () => {
     cy.visit(URL);
-    cy.request("/").then((response) => {
+    cy.request('/').then((response) => {
       expect(response.status).to.equal(200);
-      expect(response).to.have.property("headers");
-      cy.log("headers", response.headers);
+      expect(response).to.have.property('headers');
+      cy.log('headers', response.headers);
 
       // headers are lowercase
-      expect(response.headers).to.have.property("server");
-      expect(response.headers["server"]).to.contain("static-lite-server:");
+      expect(response.headers).to.have.property('server');
+      expect(response.headers['server']).to.contain('static-lite-server:');
     });
   });
 
-  it("Check default 404 page", () => {
+  it('Check default 404 page', () => {
     cy.visit(URL);
     cy.request({ url: NF_URL, failOnStatusCode: false }).then((response) => {
       expect(response.status).to.equal(404);
 
-      expect(response.body).to.contain("Not Found (404)");
+      expect(response.body).to.contain('Not Found (404)');
     });
   });
 });
